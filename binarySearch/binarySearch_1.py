41d826420baaa7cdfc483fdfb026d0d2bf49cb3e
Vincent L <vincentluoster93@gmail.com>
2018-06-12 18:10:29 -0700

    binarysearch1

---
+++ binarySearch/binarySearch_1.py
@@ -0,0 +1,25 @@
+# Binary Search Template 1
+def binarySearch(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    if len(nums) == 0:
+        return -1
+
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    # End Condition: left > right
+    return -1
+
+testArray = [1, 5, 6, 10, 25, 35, 100, 200, 800]
+print(binarySearch(testArray, 200))

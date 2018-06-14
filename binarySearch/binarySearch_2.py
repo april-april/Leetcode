112969d0f7dadf7485d5809fa62f55a1a93fca2c
Vincent L <vincentluoster93@gmail.com>
2018-06-13 22:48:39 -0700

    binary search #2

---
+++ binarySearch/binarySearch_2.py
@@ -0,0 +1,31 @@
+# template 2 for binary search
+'''
+Template #2 is an advanced form of Binary Search. 
+It is used to search for an element or condition which requires 
+accessing the current index and its immediate right neighbor's index in the array.
+ '''
+
+def binarySearch(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    if len(nums) == 0:
+        return -1
+
+    left, right = 0, len(nums)
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid
+
+    # Post-processing:
+    # End Condition: left == right
+    if left != len(nums) and nums[left] == target:
+        return left
+    return -1

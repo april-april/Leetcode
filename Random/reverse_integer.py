8be1808afc4dffe4fb1b8cd0090443dec42208f5
Vincent L <vincentluoster93@gmail.com>
2018-07-04 19:42:33 -0700

    update and test

--- Random/reverse_integer.py
+++ Random/reverse_integer.py
@@ -9,10 +9,15 @@ Examples:
 def reverse_integer(x):
 
   reversed = 0
+  #sign if it is positive vs negative
   sign = 1 if x > 0 else -1
+  tmp = abs(x)
 
   while tmp != 0:
     reversed = reversed * 10 + tmp % 10
     tmp = tmp // 10
 
   return sign * reversed
+
+  #test
+print(reverse_integer(-123) == -321)

645db702d7f72f12c1022ea02acc5f0deceb5b18
Vincent L <vincentluoster93@gmail.com>
2018-07-04 19:22:11 -0700

    optimized solution

--- Random/two_sum.py
+++ Random/two_sum.py
@@ -6,5 +6,18 @@ def two_sum(numbers, target):
     for j in range(i + 1, len(numbers)):
       if numbers[i] + numbers[j] == target:
         return True
-        
+
+  return False
+
+#optimized O(n)
+
+def two_sum2(numbers, target):
+
+  s = set()
+
+  for num in numbers:
+    if (target - num) in s:
+      return True
+    s.add(num)
+    
   return False

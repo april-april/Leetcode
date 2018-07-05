30d05e850f5752a22c5c0bedfb30f28e1be1f9ba
Vincent L <vincentluoster93@gmail.com>
2018-07-04 19:53:04 -0700

    linked_list_sum_tbc

---
+++ Random/linked_list_sum.py
@@ -0,0 +1,16 @@
+"""
+You are given two non-empty linked lists that represent integers. A linked
+list of 1->2->3 represents the integer 321. Write a function to sum two of
+these linked lists and return the sum as a linked list. For example, given
+the linked lists, 1->2->3 and 2->4->7, the sum is 3->6->0->1.
+
+"""
+
+class Node:
+
+  def __init__(self, value=0, next=None):
+    self.value = value
+    self.next = next
+    
+def linked_list_sum_iterative(list1, list2):
+    print('to bo continued')

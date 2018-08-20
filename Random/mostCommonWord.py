efc8473c7ac7dfda55d1df47a3788371ce4c95b2
Vincent L <vincentluoster93@gmail.com>
2018-08-20 00:00:01 -0700

    update

--- Random/mostCommonWord.py
+++ Random/mostCommonWord.py
@@ -6,7 +6,7 @@ Create new paragraph containing word that are not in banned list,
 Calculate number of appereance of each word in new para and save it in a map,
 Return the word from map with highest occurence value """
 
-def mostCommonWord(self, paragraph, banned):
+def mostCommonWord(paragraph, banned):
 
         ban = set(banned)
         paragraph = [s.strip("!?',;.") for s in paragraph.lower().split(' ')]        

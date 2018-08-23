845223bdf34e4c741d82db83da0f0c70e2fff29f
Vincent L <vincentluoster93@gmail.com>
2018-08-22 23:59:05 -0700

    linkedlist has cycle

---
+++ Random/linked-list_has_cycle.py
@@ -0,0 +1,10 @@
+def hasCycle(head):
+    try:
+        slow = head
+        fast = head.next
+        while slow is not fast:
+            slow = slow.next
+            fast = fast.next.next
+        return True
+    except:
+        return False

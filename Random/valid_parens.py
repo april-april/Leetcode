ac3aa3b7dc200dcb7a8e0d658a439fc1118d6a1a
Vincent L <vincentluoster93@gmail.com>
2018-08-29 23:35:03 -0700

    better way

--- Random/valid_parens.py
+++ Random/valid_parens.py
@@ -23,3 +23,29 @@ class Solution(object):
                 return False
             
         return stack == []
+
+
+''' 
+Add left handed parentheses to the stack as they are encountered. 
+If we encounter a right hand parentheses, 
+check if the stack is empty (meaning we didn't previously encounter its match) 
+or if the stack's top element is its pair. 
+At the end return True if the stack is empty
+'''
+
+class Solution2(object):
+    def isValid2(self, s):
+        d = {'{':'}', '(':')', '[':']'}
+        stack = []
+        for parens in s:
+            if parens in d:
+                stack.append(parens)
+            elif not stack or parens != d[stack.pop()]:
+                return False
+        return not stack
+
+if __name__ == '__main__':
+    solution2 = Solution2()
+    testString = '[([()])]'
+    print(solution2.isValid2(testString))
+

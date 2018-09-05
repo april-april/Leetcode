85574c3e5cca3dbb653eacadcee51a3ae7f4771a
Vincent L <vincentluoster93@gmail.com>
2018-09-04 23:20:46 -0700

    merge bt

--- loleetcode/merge_binary-tree.py
+++ loleetcode/merge_binary-tree.py
@@ -1,3 +1,10 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution(object):
     def mergeTrees(self, t1, t2):
         """
@@ -10,5 +17,14 @@ class Solution(object):
             t1.left = self.mergeTrees(t1.left, t2.left)
             t1.right = self.mergeTrees(t1.right, t2.right)
             return t1
+        else:
+            return t1 or t2
+
+    def mergeTrees2(self, t1, t2):
+        if t1 and t2:
+            root = TreeNode(t1.val + t2.val)
+            root.left = self.mergeTrees2(t1.left, t2.left)
+            root.right = self.mergeTrees2(t1.right, t2.right)
+            return root
         else:
             return t1 or t2

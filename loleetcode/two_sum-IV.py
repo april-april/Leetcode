78ce192ded0dd4505c60d95ecb770931754ba9ad
Vincent L <vincentluoster93@gmail.com>
2018-09-07 02:25:40 -0700

    beautiful solution

--- loleetcode/two_sum-IV.py
+++ loleetcode/two_sum-IV.py
@@ -16,5 +16,26 @@ class Solution:
         elif (k - root.val) in dict:
             return True
         dict.add(root.val)
-        
+
         return self.find(root.left, k, dict) or self.find(root.right, k, dict)
+    
+class Solution2(object):
+    def findTarget(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+        bfs, s = [root], set()
+        
+        for i in bfs:
+            if k - i.val in s:
+                return True
+            s.add(i.val)
+            if i.left:
+                bfs.append(i.left)
+            if i.right:
+                bfs.append(i.right)
+        return False

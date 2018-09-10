d6aa1581ed6d621af064f2a83188a31a5feb6ae4
Vincent L <vincentluoster93@gmail.com>
2018-09-09 17:17:28 -0700

    is same tree

---
+++ loleetcode/same_tree.py
@@ -0,0 +1,23 @@
+class Node:
+	# A utility function to create a new node
+	def __init__(self ,key):
+		self.data = key
+		self.left = None
+		self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        elif p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+

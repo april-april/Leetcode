34a74a3596b9f4fc3ba2cc12f0598d2c9b2a1d1c
Vincent L <vincentluoster93@gmail.com>
2018-09-10 22:39:14 -0700

    merge sorted ll

---
+++ loleetcode/merge_sorted-ll.py
@@ -0,0 +1,31 @@
+def merge_sorted(head1, head2):
+
+
+    mergedHead = None
+    if head1.data <= head2.data:
+        mergedHead = head1
+        head1 = head1.next
+    else:
+        mergedHead = head2
+        head2 = head2.next
+
+    mergedTail = mergedHead
+  
+    while head1 != None and head2 != None:
+        temp = None
+        if head1.data <= head2.data:
+            temp = head1
+            head1 = head1.next
+        else:
+            temp = head2
+            head2 = head2.next
+
+    mergedTail.next = temp
+    mergedTail = temp
+
+    if head1 != None:
+        mergedTail.next = head1
+    elif head2 != None:
+        mergedTail.next = head2
+
+    return mergedHead
